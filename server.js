9cecbf2e8ae6805558de39a58c9afb0fd2ecc61e
DebugIt <ronakmithiya@gmail.com>
2025-06-14 15:29:39 +0530

    lfgh

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.get('/', (req, res) => {
   res.send('🎭 Welcome to Initial Route');
 });
 
+app.get("/_/health", async (req, res) => {
+    return res.status(200).json({
+        message: "Health good :)"
+    })
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server up and running on http://localhost:${PORT}`);
